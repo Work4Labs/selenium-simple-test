14db51db1bc7a59b9f63b3ebe02f9e88c31ea87b
Michael Foord <michael.foord@canonical.com>
2010-10-28 17:33:56 -0400

    Addition of waitfor action

--- src/funcrunner/actions.py
+++ src/funcrunner/actions.py
@@ -1,9 +1,11 @@
+import time
+
 from selenium import FIREFOX
 from selenium.remote import connect
 
 
 __all__ = [
-    'start', 'stop', 'title_is', 'goto'
+    'start', 'stop', 'title_is', 'goto', 'waitfor'
 ]
 
 browser = None
@@ -46,3 +48,24 @@ def title_is(title):
         print msg
 
     assert title == real_title, msg
+
+"""
+# Example action using waitfor
+def wait_for_title_to_change(title):
+    def title_changed():
+        return browser.get_title() == title
+
+    waitfor(title_changed, 'title to change')
+"""
+
+def waitfor(condition, msg='', timeout=5, poll=0.1):
+    start = time.time()
+    max_time = time.time() + timeout
+    while True:
+        if condition():
+            break
+        if time.time() > max_time:
+            error = 'Timed out waiting for: ' + msg
+            _print(error)
+            raise AssertionError(error)
+        time.sleep(poll)
