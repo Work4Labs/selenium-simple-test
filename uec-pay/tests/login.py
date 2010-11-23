94616cca9551df17e4cfadc78da709e080c96025
Michael Foord <michael.foord@canonical.com>
2010-11-23 04:20:25 -0800

    Extending pay.uec login test

--- uec-pay/tests/login.py
+++ uec-pay/tests/login.py
@@ -3,7 +3,7 @@ from funcrunner.actions import *
 # passwords.py must exist in the uec-pay directory
 from passwords import username, password
 
-set_base_url('http://uec.pay/')
+set_base_url('http://pay.uec/')
 goto('/')
 
 is_link(get_element(text='Log in'))
@@ -16,11 +16,15 @@ is_textfield('id_email')
 is_textfield('id_password')
 textfield_write('id_email', username)
 textfield_write('id_password', password)
-sleep(10)
 
-button_click(get_element(text='Continue'))
-sleep(10)
+button_click(get_element(css_class='btn', name='continue'))
+
+url_contains('https://login.staging.launchpad.net/')
+title_is('Authenticate to http://pay.uec/')
+button_click(get_element(css_class='btn', name='yes'))
+
 
 url_is('http://pay.uec/payment/')
 
 get_element(tag='h1', text="Your payment history")
+sleep(20)
