41bb98a7b3e9c48f28fa982d5316a853198dd3ea
Michael Foord <michael.foord@canonical.com>
2011-02-04 17:06:40 +0000

    Extend pay test

--- staging-pay/tests/newpayment.py
+++ staging-pay/tests/newpayment.py
@@ -1,9 +1,21 @@
 from funcrunner.actions import *
 from _actions import login
 
+# login to payments
 login()
 
 # testconsumer
 goto('/test/')
+
+get_element(tag='h2', text='Welcome to the Test Consumer!')
+get_element(tag='h2', text='Create a new payment')
+
 link_click(get_element(text='Click here'))
-sleep(20)
+url_is('/test/new/')
+
+get_element(tag='h2', text='New payment')
+
+item_description="""A funky foobar
+With several lines of description
+like this"""
+textfield_write('1-item_description', item_description)
