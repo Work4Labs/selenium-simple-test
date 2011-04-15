3f3d636910bc2f2e2caa40fd36bdbe2521d6955b
Corey Goldberg <corey.goldberg@canonical.com>
2011-04-15 10:26:39 -0400

    added tests for get_element

--- tests/get_element.py
+++ tests/get_element.py
@@ -2,6 +2,43 @@ from funcrunner.actions import *
 
 goto('/')
 
-elem = get_element(tag='td', text='Get text from TD')
-print elem.text
+# need at least one parameter
+fails(get_element)
 
+elem = get_element(tag='title')
+text_is(elem, "The Page Title")
+
+# should fail if no elements match
+fails(get_element, tag="foobar")
+
+# should fail if more than one element found
+fails(get_element, tag='p')
+
+get_element(id='radio_with_id_1')
+get_element(tag='input', id='radio_with_id_1')
+
+fails(get_element, id="foobar")
+
+get_element(css_class="unique_class")
+get_element(css_class="unique_class", id="some_id")
+get_element(css_class="unique_class", id="some_id", tag="p")
+fails(get_element, css_class="some_class")
+fails(get_element, css_class="foobar")
+
+# checking arbitrary attributes
+get_element(value="unique")
+fails(get_element, value="first")
+
+elem = get_element(type='radio', name='radio_with_id', checked="1")
+is_radio(elem)
+
+# finding by text
+elem = get_element(tag='h2', text='Foo bar baz')
+text_is(elem, 'Foo bar baz')
+elem = get_element(text='Foo bar baz')
+text_is(elem, 'Foo bar baz')
+
+get_element(tag='td', text='Get text from TD')
+
+# should fail for a partial match
+fails(get_element, text='Foo bar')
