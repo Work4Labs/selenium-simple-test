748a2e841f1a35da6157deec8097b937589b0dbb
Corey Goldberg <corey.goldberg@canonical.com>
2011-05-20 08:08:10 -0400

    added LGPLv3 license headers

--- src/funcrunner/actions.py
+++ src/funcrunner/actions.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+#
+#   Copyright (c) 2011 Canonical Ltd.
+# 
+#   This file is part of: SST (selenium-simple-test)
+#   https://launchpad.net/selenium-simple-test
+#
+#   License: GNU LGPLv3 (http://www.gnu.org/licenses/)
+#
+#   SST is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Lesser Public License 
+#   as published by the Free Software Foundation.
+#
+
 
 """
 Tests are comprised of Python scripts in a "tests" directory. Files whose names
