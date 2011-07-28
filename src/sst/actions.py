3e5755c5e6f9a5f15a4a4d65174f3baedfb97bf0
Michael Foord <michael.foord@canonical.com>
2011-07-28 18:28:30 +0100

    Tests now have timeout set. Docstring for waitfor and set_wait_timeout

--- src/sst/actions.py
+++ src/sst/actions.py
@@ -294,7 +294,14 @@ _TIMEOUT = 5
 _POLL = 0.1
 
 def set_wait_timeout(timeout, poll=None):
-    """Set the timeout used by`waitfor`"""
+    """
+    Set the timeout, in seconds, used by`waitfor`. The default at the start of
+    a test is always 5 seconds.
+
+    The optional second argument, is how long (in seconds) `waitfor` should
+    wait in between checking its condition (the poll frequency). The default
+    at the start of a test is always 0.1 seconds.
+    """
     global _TIMEOUT
     global _POLL
     _TIMEOUT = timeout
@@ -306,11 +313,16 @@ def set_wait_timeout(timeout, poll=None):
 def waitfor(condition, *args, **kwargs):
     """
     Wait for an action to pass. Useful for checking the results of actions that
-    may take some time to complete. This action takes a condition function
-    and calls it until it returns True. The maximum amount of time it will wait
-    is specified as the timeout (default 5 seconds). The function is called at
-    intervals specified by the poll argument (default every 0.1 seconds).
+    may take some time to complete.
 
+    This action takes a condition function and any arguments it should be called
+    with. The condition function can either be an action or a function that
+    returns True for success and False for failure. For example::
+
+        waitfor(title_is, 'Some page title')
+
+    If the specified condition does not become true within 5 seconds then `waitfor`
+    fails. You can set the timeout for `waitfor` by calling `set_wait_timeout`.
     """
     global VERBOSE
     original = VERBOSE
