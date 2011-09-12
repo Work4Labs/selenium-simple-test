b9f9f545d8b763e1a833adb770e9d38e33152b62
julien.funk@canonical.com <>
2011-09-12 14:37:04 -0400

    fixed the test for click.py

--- src/sst/selftests/click.py
+++ src/sst/selftests/click.py
@@ -1,6 +1,13 @@
+from sst.actions import *
+
 goto('/')
 
 # checks that clicking works at the element level as well
 element_click(get_element(id='the_band_link'), wait=False)
 url_is('/begin')
 
+goto('/')
+
+# checks the wait option 
+element_click(get_element(id='the_band_link'), wait=True)
+url_is('/begin')
