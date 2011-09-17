66b7635d69d2edcb4c318441e5ecd28cbfc45645
Corey Goldberg <corey.goldberg@canonical.com>
2011-09-17 17:58:47 -0400

    removed the invalid self-test from textfield

--- src/sst/selftests/textfield.py
+++ src/sst/selftests/textfield.py
@@ -18,24 +18,20 @@ fails(is_textfield, 'foobar')
 # fails for element that exists but isn't a textfield
 fails(is_textfield, 'radio_with_id_1')
 
-textfield_write('text_1', "I pity the Foobar..")
+textfield_write('text_1', 'I pity the Foobar..')
 text_is('text_1', "I pity the Foobar..")
 
-textfield_write('text_1', "Overwriting")
+textfield_write('text_1', 'Overwriting')
 text_is('text_1', "Overwriting")
 
-textfield_write('text_1', "No checking", check=False)
-text_is('text_1', "No checking")
+textfield_write('text_1', 'No checking', check=False)
+text_is('text_1', 'No checking')
 
 # check with empty text
-textfield_write('text_1', "")
-text_is('text_1', "")
+textfield_write('text_1', '')
+text_is('text_1', '')
 
 # checks the password field to see if it is editable
-textfield_write('pass_1', "qaT3st")
-text_is('pass_1', "qaT3st")
+textfield_write('pass_1', 'qaT3st')
+text_is('pass_1', 'qaT3st')
 fails(text_is, 'pass_1', 'fake_text')
-
-# fails for element that exists but is not editable
-fails(textfield_write, 'text_2', "I pity the Foobar..")  # TODO: this line line is failing in Chrome
-fails(text_is, 'text_2', "I pity the Foobar..")
