c27484e12ed364520596d980f182b7856b9b3214
Corey Goldberg <corey.goldberg@canonical.com>
2011-09-24 12:10:45 -0400

    removed negative (fails) tests from get_element.py

--- src/sst/selftests/get_element.py
+++ src/sst/selftests/get_element.py
@@ -4,9 +4,6 @@ goto('/')
 title_is('The Page Title')
 
 
-# need at least one parameter
-fails(get_element)
-
 # simple title check
 elem = get_element(tag='title')
 text_is(elem, 'The Page Title')
@@ -45,14 +42,23 @@ elem = get_element(href='/longscroll')
 text_is(elem, 'link to longscroll page')
 
 
+# find by css class
+get_element(css_class='unique_class')
+get_element(css_class='unique_class', id='some_id')
+get_element(css_class='unique_class', id='some_id', tag='p')
 
-# should fail if no elements match
-fails(get_element, tag='foobar')
-fails(get_element, id='foobar')
-fails(get_element, tag='foobar', id='foobar')
+# checking arbitrary attributes
+get_element(value='unique')
 
-# should fail if more than one element found
-fails(get_element, tag='p')
+# find by text
+elem = get_element(tag='h2', text='Foo bar baz')
+text_is(elem, 'Foo bar baz')
+elem = get_element(text='Foo bar baz')
+text_is(elem, 'Foo bar baz')
+
+# find table row by text
+elem = get_element(tag='td', text='Get text from TD')
+text_is(elem, 'Get text from TD')
 
 # radio buttons
 elem = get_element(id='radio_with_id_1')
@@ -62,25 +68,4 @@ is_radio(elem)
 elem = get_element(type='radio', name='radio_with_id', checked='1')
 is_radio(elem)
 
-# find by css class
-get_element(css_class='unique_class')
-get_element(css_class='unique_class', id='some_id')
-get_element(css_class='unique_class', id='some_id', tag='p')
-fails(get_element, css_class='some_class')
-fails(get_element, css_class="foobar")
 
-# checking arbitrary attributes
-get_element(value='unique')
-fails(get_element, value='first')
-
-# find by text
-elem = get_element(tag='h2', text='Foo bar baz')
-text_is(elem, 'Foo bar baz')
-elem = get_element(text='Foo bar baz')
-text_is(elem, 'Foo bar baz')
-
-# find table row by text
-get_element(tag='td', text='Get text from TD')
-
-# should fail for a partial match
-fails(get_element, text='Foo bar')
