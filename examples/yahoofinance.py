d85e8cea8e4b5eed0e4db257473f60972500d92c
Corey Goldberg <corey.goldberg@canonical.com>
2011-11-28 12:51:15 -0500

    added option to disable text clearing on textfield_write()

--- examples/yahoofinance.py
+++ examples/yahoofinance.py
@@ -1,9 +1,10 @@
 from sst.actions import *
 
+
 goto('http://finance.search.yahoo.com/')
 title_contains('Yahoo!')
 element = get_element(id='yschsp')
-textfield_write(element, 'IBM')
+textfield_write(element, 'AMZN', clear=False)
 element = get_element(id='yschbt')
 button_click(element)
-title_contains('IBM')
+title_contains('AMZN - Yahoo')
