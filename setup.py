b75619831ebc826c6957acdaa16e093afeeb4fad
Corey Goldberg <corey.goldberg@canonical.com>
2012-01-01 13:33:37 -0500

    added selftests back to setup.py

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ params = dict(
     version=__version__,
     packages=PACKAGES,
     scripts=SCRIPTS,
-    package_dir={'': 'src'},
+    package_dir={'': 'src', 'selftests': 'src/selftests'},
     install_requires = REQUIREMENTS,
     
     # metadata for upload to PyPI
