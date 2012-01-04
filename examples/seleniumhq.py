4d5ab53e261fedc60dd27ceef4e881e04dd0c7b5
Corey Goldberg <corey.goldberg@canonical.com>
2012-01-04 13:05:49 -0500

    wait_for displays traceback for exceptions during waits

--- examples/seleniumhq.py
+++ examples/seleniumhq.py
@@ -1,5 +1,4 @@
 from sst.actions import *
 
 go_to('http://seleniumhq.org/')
-#assert_title('Selenium - Web Browser Automation')
-wait_for(assert_title, 'Selenium - Web Browser Automation')
+assert_title('Selenium - Web Browser Automation')
