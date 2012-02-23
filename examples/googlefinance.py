7d9b84c4355df776c7a3369ef1e1b37f755ff961
Corey Goldberg <corey.goldberg@canonical.com>
2012-02-23 11:13:41 -0500

    fixed example scripts (yahoofinance, googlefinance)

--- examples/googlefinance.py
+++ examples/googlefinance.py
@@ -1,18 +1,16 @@
 from sst.actions import *
 
 
-go_to('http://www.google.com/')
+set_base_url('http://www.google.com/')
+
+go_to('/')
 assert_title_contains('Google')
 
-set_base_url('http://www.google.com/')
 go_to('/finance')
-
 assert_url('/finance')
-fails(assert_url, '/foo')
-
 assert_title_contains('Google Finance: Stock market')
 
 write_textfield(get_element(name='q'), 'IBM')
-click_button(get_element(tag='input', value='Get quotes'))
-
+click_button('gbqfb')
 assert_url_contains('IBM')
+assert_title_contains('International Business Machines')
