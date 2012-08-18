83f967b9cc594d5c1859c85ea1c8812d4e392d19
Corey Goldberg <cgoldberg@gmail.com>
2012-08-18 16:03:11 -0400

    fixed admin tests

--- src/sst/selftests/admin_load.py
+++ src/sst/selftests/admin_load.py
@@ -7,4 +7,13 @@ import helpers
 helpers.skip_as_jenkins()
 
 go_to('/admin/')
+assert_title_contains('Django site admin')
+
+# logout of Admin if needed
+elem = get_element(tag='title')
+if 'Log in' not in elem.text:
+    click_link(get_element(text='Log out'))
+    assert_title('Logged out | Django site admin')
+    refresh()
+
 assert_title('Log in | Django site admin')
--- src/sst/selftests/admin_login.py
+++ src/sst/selftests/admin_login.py
@@ -7,6 +7,15 @@ import helpers
 helpers.skip_as_jenkins()
 
 go_to('/admin/')
+assert_title_contains('Django site admin')
+
+# logout of Admin if needed
+elem = get_element(tag='title')
+if 'Log in' not in elem.text:
+    click_link(get_element(text='Log out'))
+    assert_title('Logged out | Django site admin')
+    refresh()
+
 assert_title('Log in | Django site admin')
 
 # login to Admin site
@@ -18,3 +27,7 @@ assert_element(tag='h1', id='site-name', text='Django administration')
 
 # make sure you didn't get bounced back to login page
 fails(assert_title, 'Log in | Django site admin')
+
+#logout
+click_link(get_element(text='Log out'))
+assert_title('Logged out | Django site admin')
