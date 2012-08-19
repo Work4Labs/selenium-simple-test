915ee263fff6e8c003fb6164c64886455041631f
Corey Goldberg <cgoldberg@gmail.com>
2012-08-19 10:25:37 -0400

    added admin_cookies.py test for get_cookies(), using internal testproject Admin site

---
+++ src/sst/selftests/admin_cookies.py
@@ -0,0 +1,40 @@
+from sst.actions import *
+
+import helpers
+
+
+# skip this test when running from CI
+helpers.skip_as_jenkins()
+
+go_to('/admin/')
+assert_title_contains('Django site admin')
+
+# logout of Admin if needed
+elem = get_element(tag='title')
+if 'Log in' not in elem.text:
+    click_link(get_element(text='Log out'))
+    assert_title('Logged out | Django site admin')
+    refresh()
+
+assert_title('Log in | Django site admin')
+
+# login to Admin site
+write_textfield('id_username', 'sst')
+write_textfield('id_password', 'password')
+click_element(get_element(value='Log in'))
+assert_title('Site administration | Django site admin')
+assert_element(tag='h1', id='site-name', text='Django administration')
+
+# make sure you didn't get bounced back to login page
+fails(assert_title, 'Log in | Django site admin')
+
+# get cookies of current session (set of dicts)
+cookies = get_cookies()
+assert len(cookies) > 0
+for cookie in cookies:
+    assert (cookie['name'] in ('csrftoken', 'sessionid'))
+    assert (len(cookie['value']) > 1)
+
+# logout
+click_link(get_element(text='Log out'))
+assert_title('Logged out | Django site admin')
