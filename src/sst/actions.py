86bbda158d5936985f87802e940433f08b8c7ae5
Corey Goldberg <cgoldberg@gmail.com>
2012-08-27 12:11:28 -0400

    fixed assert_url

--- src/sst/actions.py
+++ src/sst/actions.py
@@ -646,8 +646,9 @@ def assert_url(url):
     Assert the current url is as specified. Can be an absolute url or
     relative to the base url."""
     url = _fix_url(url)
-    #url = _add_trailing_slash(url)
+    url = _add_trailing_slash(url)
     real_url = browser.current_url
+    real_url = _add_trailing_slash(real_url)
     msg = 'Url is: %r. Should be: %r' % (real_url, url)
     if url != real_url:
         _raise(msg)
