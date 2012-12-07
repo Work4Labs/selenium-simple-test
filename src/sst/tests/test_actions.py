6dc8c81a07656d1645044d285fed28a3b7e3b7ce
Vincent Ladeuil <vila+qa@canonical.com>
2012-12-07 17:27:34 +0100

    Refactor tests for easier understanding.

--- src/sst/tests/test_actions.py
+++ src/sst/tests/test_actions.py
@@ -10,48 +10,38 @@ from sst import actions
 
 class TestRetryOnStale(testtools.TestCase):
 
+    def setUp(self):
+        super(TestRetryOnStale, self).setUp()
+        self.out = StringIO()
+        # Capture output from retry_on_stale_element calls
+        self.patch(sys, 'stdout', self.out)
+        self.nb_calls = 0
+
+    def raise_stale_element(self):
+        self.nb_calls += 1
+        if self.nb_calls == 1:
+            raise actions.StaleElementReferenceException('whatever')
+        return 'success'
+
+    def assertRaisesOnlyOnce(self, expected, func, *args):
+        # Calling the function succeeds
+        self.assertEqual(expected, func(*args))
+        # But under the hood it's been called twice
+        self.assertEqual(2, self.nb_calls)
+        # And we get some feedback about the exception
+        self.assertIn(
+            '    Retrying after catching: StaleElementReferenceException()\n',
+            self.out.getvalue())
+
     def test_retry_on_stale_only_once(self):
         """retry once on StaleElementReferenceException."""
-        self.nb_calls = 0
-
         @actions.retry_on_stale_element
-        def raiser():
-            self.nb_calls += 1
-            if self.nb_calls == 1:
-                raise actions.StaleElementReferenceException('whatever')
-            return 'success'
+        def protected_raiser():
+            return self.raise_stale_element()
 
-        out = StringIO()
-        self.patch(sys, 'stdout', out)
-        # Calling the function succeeds
-        self.assertEqual('success', raiser())
-        # But under the hood it's been called twice
-        self.assertEqual(2, self.nb_calls)
-        # And we get some feedback about the exception
-        self.assertEqual(
-            '    Retrying after catching: StaleElementReferenceException()\n',
-            out.getvalue())
+        self.assertRaisesOnlyOnce('success', protected_raiser)
 
     def test_wait_for_retries(self):
-        self.nb_calls = 0
-
-        def raiser():
-            self.nb_calls += 1
-            if self.nb_calls == 1:
-                raise actions.StaleElementReferenceException('whatever')
-            return True
-
-        out = StringIO()
-        self.patch(sys, 'stdout', out)
-        # Calling the function succeeds
-        self.assertEqual(None, actions.wait_for(raiser))
-        # But under the hood it's been called twice
-        self.assertEqual(2, self.nb_calls)
-        # And we get some feedback about the exception
-        self.assertEqual('''\
-    Waiting for 'raiser'
-    Retrying after catching: StaleElementReferenceException()
-    Waiting for 'raiser'
-''',
-            out.getvalue())
+        self.assertRaisesOnlyOnce(None, actions.wait_for,
+                                  self.raise_stale_element)
 
