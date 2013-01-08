e8fe0719378d15146809054393222a0d83831d10
Corey Goldberg <cgoldberg@gmail.com>
2013-01-08 18:16:11 -0500

    fixed test for maximize

--- src/sst/selftests/window_maximize.py
+++ src/sst/selftests/window_maximize.py
@@ -1,6 +1,8 @@
 from sst.actions import *
 
 
+go_to('/')
+
 before_width = 300
 before_height = 200
 
@@ -12,10 +14,10 @@ assert width > before_width
 assert height > before_height
 
 # switch to new window/tab and maximize it
-go_to('/')
 click_link('popup_link')
 switch_to_window(index_or_name='_NEW_WINDOW')
 assert_title('Popup Window')
+set_window_size(before_width, before_height)
 maximize_window()
 width, height = get_window_size()
 assert width > before_width 
