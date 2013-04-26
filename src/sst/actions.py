b2bfb6f9221a4782ce26447313721c1cb703c9b1
Guilhaume Bordiau <gbordiau@work4labs.com>
2013-04-26 12:42:17 +0200

    Share the browser also when not on remote

--- src/sst/actions.py
+++ src/sst/actions.py
@@ -238,9 +238,9 @@ def start(browser_type=None, browser_version='',
         browser = browser_class(desired_capabilities=desired_capabilities,
                                 command_executor=webdriver_remote)
 
-        # FIXME: Not a good way to share the browser object.
-        config.cache['browser'] = browser
-        config.cache['browsermob_proxy'] = browsermob_proxy
+    # FIXME: Not a good way to share the browser object.
+    config.cache['browser'] = browser
+    config.cache['browsermob_proxy'] = browsermob_proxy
 
     return browser, browsermob_proxy
 
