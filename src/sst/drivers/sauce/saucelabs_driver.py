1a522e409a72d651980c7c953519e70fb0898f72
Peter Penkalla <ppenkalla@work4labs.com>
2013-10-07 16:38:58 +0200

    Update saucelabs_driver.py
    Upgraded to 180sec

--- src/sst/drivers/sauce/saucelabs_driver.py
+++ src/sst/drivers/sauce/saucelabs_driver.py
@@ -28,7 +28,7 @@ class SauceLabsDriver(webdriver.Remote):
 
         default_capabilities = {
             'record-video': 'false',
-            'max-duration': '120'
+            'max-duration': '180'
         }
 
         # Extend the default capabilities with the custom ones
