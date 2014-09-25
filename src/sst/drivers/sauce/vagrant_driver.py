8ba8f0d967863e1cac5b2bc81756a49ad8661816
Emile Ré <nemile.re@gmail.com>
2014-09-25 16:43:53 +0200

    YOMO-7514 added vagrant driver file

---
+++ src/sst/drivers/sauce/vagrant_driver.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+class VagrantDriver(webdriver.Remote):
+
+    def get_job_result_url(self):
+        return
+
+    def job_read(self, job_id):
+        return
+
+    def job_update(self, result, exception=None):
+        return
+
+    def job_list(self, result):
+        return
